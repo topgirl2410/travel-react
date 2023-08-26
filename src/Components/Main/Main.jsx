5db77be1939c4b334ffd5abc5535a30827a17a87
topgirl2410 <maria.topgirl82@gmail.com>
2023-08-26 21:32:05 +0200

    finish

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -10,15 +10,15 @@ import { BsClipboardCheckFill } from 'react-icons/bs'
 
 // import the images
 
-import img10 from '../../assets/img10.jpg'
-import img6 from '../../assets/img6.jpg'
-import img11 from '../../assets/img11.jpg'
-import img9 from '../../assets/img9.jpg'
-import img12 from '../../assets/img12.jpg'
-import img3 from '../../assets/img3.jpg'
-import img5 from '../../assets/img5.jpg'
+import img1 from '../../assets/img1.jpg'
 import img2 from '../../assets/img2.jpg'
+import img3 from '../../assets/img3.jpg'
+import img4 from '../../assets/img4.jpg'
+import img5 from '../../assets/img5.jpg'
+import img6 from '../../assets/img6.jpg'
+import img7 from '../../assets/img7.jpg'
 import img8 from '../../assets/img8.jpg'
+import img9 from '../../assets/img9.jpg'
 
 
 
@@ -27,84 +27,84 @@ import img8 from '../../assets/img8.jpg'
 const Data = [
     {
         id: 1,
-        imgSrc: img10,
+        imgSrc: img1,
         destTitle: 'Vacations',
         location: 'New Zeland',
         grade: 'CULTURAL RELAX',
         fees: '1000€',
-        description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is know for its luxurius stays and adventurous activities',
+        description: 'Nunc efficitur odio sit amet lectus faucibus dapibus. Vivamus rhoncus eros vitae elit ullamcorper, non imperdiet dui viverra. Nam eleifend eros nulla, non euismod ipsum hendrerit a. Nam a erat eu turpis tincidunt accumsan eget vel sem. Maecenas rutrum neque eu ligula interdum faucibus. Cras ac justo varius, vulputate ipsum sit amet, ultrices nibh. Vestibulum faucibus ullamcorper ex, et tempor enim. Etiam sed cursus nisl, a gravida metus. Aenean rhoncus tristique neque a venenatis.',
     },
     {
         id: 2,
-        imgSrc: img6,
+        imgSrc: img2,
         destTitle: 'Incredible Vacations',
         location: 'Bavaro, Punta Cana',
         grade: 'CULTURAL RELAX',
         fees: '1200€',
-        description: 'The sophisticated eastern region of the Dominican Republic attracts vacationers from all over the world who come to explore the natural treasures and enjoy the luxurious resorts of Punta Cana.',
+        description: 'Nunc efficitur odio sit amet lectus faucibus dapibus. Vivamus rhoncus eros vitae elit ullamcorper, non imperdiet dui viverra. Nam eleifend eros nulla, non euismod ipsum hendrerit a. Nam a erat eu turpis tincidunt accumsan eget vel sem. Maecenas rutrum neque eu ligula interdum faucibus. Cras ac justo varius, vulputate ipsum sit amet, ultrices nibh. Vestibulum faucibus ullamcorper ex, et tempor enim. Etiam sed cursus nisl, a gravida metus. Aenean rhoncus tristique neque a venenatis.',
     },
     {
         id: 3,
-        imgSrc: img11,
+        imgSrc: img3,
         destTitle: 'Aventuras Increibles',
         location: 'Maldivas',
         grade: 'CULTURAL RELAX',
         fees: '1500€',
-        description: 'This hotel is located in Male, close to several points of interest. It offers its guests the ideal place to enjoy an exciting getaway. The property is nestled in the midst of the natural beauty and splendor of the Maldives, and immerses its visitors in an atmosphere of peace and serenity. This hotel has a charming design, with radiant exteriors that blend perfectly with its surroundings.',
+        description: 'Nunc efficitur odio sit amet lectus faucibus dapibus. Vivamus rhoncus eros vitae elit ullamcorper, non imperdiet dui viverra. Nam eleifend eros nulla, non euismod ipsum hendrerit a. Nam a erat eu turpis tincidunt accumsan eget vel sem. Maecenas rutrum neque eu ligula interdum faucibus. Cras ac justo varius, vulputate ipsum sit amet, ultrices nibh. Vestibulum faucibus ullamcorper ex, et tempor enim. Etiam sed cursus nisl, a gravida metus. Aenean rhoncus tristique neque a venenatis.',
     },
     {
         id: 4,
-        imgSrc: img9,
+        imgSrc: img4,
         destTitle: 'Playas Paridisiacas',
         location: 'Rio de Janeiro',
         grade: 'CULTURAL RELAX',
         fees: '2000€',
-        description: 'You will feel at home in any of the 143 rooms with air conditioning, minibar and flat screen TV. The free Wi-Fi Internet connection will keep you in touch with your loved ones; you can also watch your favorite program on the TV with cable channels. The bathroom has a shower and free toiletries. Conveniences include a safe, blackout drapes, and a phone.',
+        description: 'Nunc efficitur odio sit amet lectus faucibus dapibus. Vivamus rhoncus eros vitae elit ullamcorper, non imperdiet dui viverra. Nam eleifend eros nulla, non euismod ipsum hendrerit a. Nam a erat eu turpis tincidunt accumsan eget vel sem. Maecenas rutrum neque eu ligula interdum faucibus. Cras ac justo varius, vulputate ipsum sit amet, ultrices nibh. Vestibulum faucibus ullamcorper ex, et tempor enim. Etiam sed cursus nisl, a gravida metus. Aenean rhoncus tristique neque a venenatis.',
     },
     {
         id: 5,
-        imgSrc: img12,
+        imgSrc: img5,
         destTitle: 'Perfecto Relax',
         location: 'Tenerife',
         grade: 'CULTURAL RELAX',
         fees: '2000€',
-        description: 'Located in the Golf Sur de Tenerife complex, overlooking the Atlantic Ocean. The Grand Muthu Golf Plaza Hotel & Spa offers a spa, gym and outdoor pools, surrounded by terraces and gardens. Grand Muthu Golf Plaza Hotel & Spa offers apartments and studios, all with air conditioning and a furnished balcony or terrace. They include a lounge with satellite TV and a well-equipped kitchenette. There is free Wi-Fi in public areas.',
+        description: 'Nunc efficitur odio sit amet lectus faucibus dapibus. Vivamus rhoncus eros vitae elit ullamcorper, non imperdiet dui viverra. Nam eleifend eros nulla, non euismod ipsum hendrerit a. Nam a erat eu turpis tincidunt accumsan eget vel sem. Maecenas rutrum neque eu ligula interdum faucibus. Cras ac justo varius, vulputate ipsum sit amet, ultrices nibh. Vestibulum faucibus ullamcorper ex, et tempor enim. Etiam sed cursus nisl, a gravida metus. Aenean rhoncus tristique neque a venenatis.',
     },
     {
         id: 6,
-        imgSrc: img3,
+        imgSrc: img6,
         destTitle: 'Aurora Boreal',
         location: 'Suiza',
         grade: 'CULTURAL RELAX',
         fees: '2500€',
-        description: 'For unparalleled relaxation, nothing like a visit to the spa, which offers massages, body treatments and facials. Hot tub and sauna are the perfect solution to relax after a day on the ski slopes. You will also find free Wi-Fi Internet connection, concierge services, and ski storage. Thanks to the free shuttle service you can get to the ski slopes comfortably and quickly.',
+        description: 'Nunc efficitur odio sit amet lectus faucibus dapibus. Vivamus rhoncus eros vitae elit ullamcorper, non imperdiet dui viverra. Nam eleifend eros nulla, non euismod ipsum hendrerit a. Nam a erat eu turpis tincidunt accumsan eget vel sem. Maecenas rutrum neque eu ligula interdum faucibus. Cras ac justo varius, vulputate ipsum sit amet, ultrices nibh. Vestibulum faucibus ullamcorper ex, et tempor enim. Etiam sed cursus nisl, a gravida metus. Aenean rhoncus tristique neque a venenatis.',
     },
     {
         id: 7,
-        imgSrc: img5,
+        imgSrc: img7,
         destTitle: 'Hotel Villa del Sol',
         location: 'Menorca',
         grade: 'CULTURAL RELAX',
         fees: '1000€',
-        description: 'The Hotel Valentin Son Bou is located on the south coast of Menorca, on a hill overlooking the beautiful beach of Son Bou. It features an indoor pool, an outdoor pool and rooms with a balcony and air conditioning. All apartments and rooms at Valentin Son Bou Hotel have tiled floors and are decorated in soft colours.',
+        description: 'Nunc efficitur odio sit amet lectus faucibus dapibus. Vivamus rhoncus eros vitae elit ullamcorper, non imperdiet dui viverra. Nam eleifend eros nulla, non euismod ipsum hendrerit a. Nam a erat eu turpis tincidunt accumsan eget vel sem. Maecenas rutrum neque eu ligula interdum faucibus. Cras ac justo varius, vulputate ipsum sit amet, ultrices nibh. Vestibulum faucibus ullamcorper ex, et tempor enim. Etiam sed cursus nisl, a gravida metus. Aenean rhoncus tristique neque a venenatis.',
     },
     {
         id: 8,
-        imgSrc: img2,
+        imgSrc: img8,
         destTitle: 'Anixi Hotel & Studios',
         location: 'Mikonos',
         grade: 'CULTURAL RELAX',
         fees: '2000€',
-        description: 'Modern hotel with swimming pool, free Wi-Fi and just 300 m from beautiful beaches Anixi Hotel is located next to one of the most fascinating beaches on the island of Mykonos, Ornos Beach, known for its crystal clear waters, golden sands and numerous bars and restaurants.',
+        description: 'Nunc efficitur odio sit amet lectus faucibus dapibus. Vivamus rhoncus eros vitae elit ullamcorper, non imperdiet dui viverra. Nam eleifend eros nulla, non euismod ipsum hendrerit a. Nam a erat eu turpis tincidunt accumsan eget vel sem. Maecenas rutrum neque eu ligula interdum faucibus. Cras ac justo varius, vulputate ipsum sit amet, ultrices nibh. Vestibulum faucibus ullamcorper ex, et tempor enim. Etiam sed cursus nisl, a gravida metus. Aenean rhoncus tristique neque a venenatis.',
     },
     {
         id: 9,
-        imgSrc: img8,
+        imgSrc: img9,
         destTitle: 'Amaneceres Inolvidables',
         location: 'Puerto Rico',
         grade: 'CULTURAL RELAX',
         fees: '2200€',
-        description: 'With an outdoor swimming pool and many other recreational facilities at your disposal, you wont have a spare minute. You also have a terrace where you can sit and contemplate the landscape. You will also find free Wi-Fi Internet access and tour assistance (ticket acquisition).',
+        description: 'Nunc efficitur odio sit amet lectus faucibus dapibus. Vivamus rhoncus eros vitae elit ullamcorper, non imperdiet dui viverra. Nam eleifend eros nulla, non euismod ipsum hendrerit a. Nam a erat eu turpis tincidunt accumsan eget vel sem. Maecenas rutrum neque eu ligula interdum faucibus. Cras ac justo varius, vulputate ipsum sit amet, ultrices nibh. Vestibulum faucibus ullamcorper ex, et tempor enim. Etiam sed cursus nisl, a gravida metus. Aenean rhoncus tristique neque a venenatis.',
     }
 
 ]
